923ca4534a5003953731140986c4b32bf5d3e3fd
badiniibrahim <sawadogo.badiniibrahim@yahoo.fr>
2023-09-29 17:51:19 +0200

    add components

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,5 +1,19 @@
+import AccountProfile from '@/components/forms/AccountProfile';
 import React from 'react';
+import { currentUser } from '@clerk/nextjs';
 
 export default async function Page() {
-  return <div className="head-text">Onboarding</div>;
+  const user = await currentUser();
+
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now, to use NexLink.
+      </p>
+      <section className="mt-9 bg-dark-2 py-10">
+        <AccountProfile user={{}} btnTitle="Continue" />
+      </section>
+    </main>
+  );
 }
